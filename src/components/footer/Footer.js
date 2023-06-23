fad404fe1695ae61b0c3c859cdd350249797631d
Paras <parasbupadhyay2003@gmail.com>
2023-06-23 17:18:29 +0530

    made responsive

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Footer.css";
 const Footer = () => {
   return (
-    <div className="footer">
+    <div className="footer-container footer">
       <h1 className="text-center">All Right Reserved &copy; Paras Upadhyay</h1>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|
