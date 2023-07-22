989d793cc646e79feaeb0443a4b72d7f8cf56166
Paras <parasbupadhyay2003@gmail.com>
2023-07-22 13:00:55 +0530

    added per page surfing

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,32 +6,90 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { AiOutlineReload } from "react-icons/ai";
 import Spinner from "../../components/spinner/Spinner";
+import Pagenotfound from "../Notfound/Pagenotfound";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
   const [loading, setLoading] = useState(true);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [load, setLoad] = useState(false);
+  const [Button, setButton] = useState(true);
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    getTotal();
   }, []);
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const res = await axios.get(
-          `${process.env.REACT_APP_API}/posts` + search
-        );
-        setPosts(res.data);
-        setLoading(false); // Set loading state to false when data is fetched
-      } catch (error) {
-        console.log(error);
-        setLoading(false); // Set loading state to false in case of an error
-      }
-    };
     fetchPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search]);
+
+  //get all posts
+  const fetchPosts = async () => {
+    try {
+      let res;
+      if (search.length) {
+        setButton(false);
+        res = await axios.get(`${process.env.REACT_APP_API}/posts` + search);
+        if (res.data.length === 0) {
+        }
+      } else {
+        setButton(true);
+        // console.log(page);
+        if (page === 1) {
+          setPosts([]);
+        }
+        res = await axios.get(
+          `${process.env.REACT_APP_API}/posts/post-list/${page}`
+        );
+      }
+      setPosts(res.data);
+      setLoading(false); // Set loading state to false when data is fetched
+    } catch (error) {
+      console.log(error);
+      setLoading(false); // Set loading state to false in case of an error
+    }
+  };
+
+  //getTOtal COunt
+  const getTotal = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/posts/post-count`
+      );
+      setTotal(data);
+      // console.log(data);
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (page === 1) return;
+    loadMore();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  //load more
+  const loadMore = async () => {
+    try {
+      setLoad(true);
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/posts/post-list/${page}`
+      );
+      setLoad(false);
+      // console.log(data);
+      setPosts([...posts, ...data]);
+    } catch (error) {
+      // console.log(error);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     const homeDiv = document.getElementById("home");
@@ -52,11 +110,34 @@ const Home = () => {
           <div>
             <Spinner />
           </div>
+        ) : posts.length === 0 ? (
+          // Render the NotFound component if no posts were found
+          <Pagenotfound />
         ) : (
           <>
             <Header />
             <div className="homePage">
-              <Posts posts={posts} />
+              <div className="left-container">
+                <Posts posts={posts} />
+                {Button && posts && posts.length < total && (
+                  <button
+                    className="btn loadmore"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setPage(page + 1);
+                    }}
+                  >
+                    {load ? (
+                      "Loading ..."
+                    ) : (
+                      <>
+                        {" "}
+                        Loadmore <AiOutlineReload />
+                      </>
+                    )}
+                  </button>
+                )}
+              </div>
               <Sidebar />
             </div>
           </>
