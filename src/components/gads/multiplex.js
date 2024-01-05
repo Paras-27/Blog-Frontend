8ae8aecd66d9ba6994d436a123eb73d9a6b82210
Paras <parasbupadhyay2003@gmail.com>
2024-01-05 23:53:35 +0530

    ads issue resolved

--- src/components/gads/multiplex.js
+++ src/components/gads/multiplex.js
@@ -22,25 +22,3 @@ export default class MultiplexAdComponent extends React.Component {
     );
   }
 }
-
-import React from "react";
-import "./style.css";
-export default class MultiplexAdComponent extends React.Component {
-  componentDidMount() {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }
-
-  render() {
-    return (
-      <div className="adcontainer">
-        <ins
-          className="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-format="autorelaxed"
-          data-ad-client="ca-pub-6606321105150999"
-          data-ad-slot="6145627987"
-        />
-      </div>
-    );
-  }
-}
