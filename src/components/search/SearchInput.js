efccd1989ac5410bc3283548a2c303f5bbf8c950
Paras <parasbupadhyay2003@gmail.com>
2024-03-08 10:44:53 +0530

    improved a bug related to search box

--- src/components/search/SearchInput.js
+++ src/components/search/SearchInput.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSearch } from "./search";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
 const SearchInput = () => {
   const [values, setValues] = useSearch();
   const navigate = useNavigate();
@@ -9,6 +10,9 @@ const SearchInput = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      if (values.keyword.trim().length === 0) {
+        return;
+      }
       const { data } = await axios.get(
         `${process.env.REACT_APP_API}/posts/search/${values.keyword}`
       );
@@ -19,6 +23,7 @@ const SearchInput = () => {
       // console.log(error);
     }
   };
+
   return (
     <div>
       <form
@@ -27,18 +32,20 @@ const SearchInput = () => {
         onSubmit={handleSubmit}
         id="form"
       >
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search For Bhajans"
-          aria-label="Search"
-          value={values.keyword}
-          onChange={(e) => setValues({ ...values, keyword: e.target.value })}
-          name="search"
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
+        <div className="search-input-container">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search For Bhajans"
+            aria-label="Search"
+            value={values.keyword}
+            onChange={(e) => setValues({ ...values, keyword: e.target.value })}
+            name="search"
+          />
+          <button type="submit" className="submit">
+            <img src="/svg/search.svg" alt="" className="icon" />
+          </button>
+        </div>
       </form>
     </div>
   );
